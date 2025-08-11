94a690da1b49d16d1dbe66c80f6b4bfdd686142f
Sourabh Zanwar <s.zanwar@reply.de>
2025-08-11 13:19:50 +0200

    added to run well with github pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
+// filepath: vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/undercover-game/',
   plugins: [react()],
 })
